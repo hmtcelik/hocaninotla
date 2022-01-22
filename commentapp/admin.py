f721a4d9f2f402240c1bfeb58c8423363be60232
hmtcelik <abhmtcelik@gmail.com>
2022-01-22 14:17:18 +0300

    create models

--- commentapp/admin.py
+++ commentapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import City, University, Faculty, Departmant, Doctor, Comment
 
 # Register your models here.
+
+admin.site.register(City)
+admin.site.register(University)
+admin.site.register(Faculty)
+admin.site.register(Departmant)
+admin.site.register(Doctor)
+admin.site.register(Comment)
+
--- commentapp/models.py
+++ commentapp/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
 # Create your models here.
+
+class City(models.Model):
+    city_name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.city_name
+    
+class University(models.Model):
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    uni_name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.uni_name
+    
+class Faculty(models.Model):
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    faculty_name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.faculty_name
+    
+class Departmant(models.Model):
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    departmant_name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.departmant_name
+    
+class Doctor(models.Model):
+    departmant = models.ForeignKey(Departmant, on_delete=models.CASCADE)
+    doctor_name = models.CharField(max_length=50)
+    doctor_image = models.FileField(blank=True)
+    about = models.TextField(max_length=500)
+    
+    def __str__(self):
+        return self.doctor_name
+    
+class Comment(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    comment_author = models.CharField(max_length=50)
+    comment_title = models.CharField(max_length=50)
+    comment = models.TextField(max_length=500)
+    
+    def __str__(self):
+        return self.doctor.doctor_name + '-' + self.comment_author
+
+
+
+
+
+    
+    
