6c02c68afb0869bf0ba562b3d6b194a30eb0560f
hmtcelik <abhmtcelik@gmail.com>
2022-01-25 18:13:51 +0300

    edit urls

--- comment/urls.py
+++ comment/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name = 'home'),
     path('university/', views.UniView.as_view(), name='uni'),
     path('university/<int:pk>/', views.FacultyView.as_view(), name='faculty'),
-    path('faculty/<int:pk>/', views.DepartView.as_view(), name='depart'),
-    path('depart/<int:pk>/', views.DoctorView.as_view(), name='doctor'),
-    path('doctor/<int:pk>/', views.CommentView.as_view(), name='comment'),
+    path('university/faculty/<int:pk>/', views.DepartView.as_view(), name='depart'),
+    path('university/faculty/depart/<int:pk>/', views.DoctorView.as_view(), name='doctor'),
+    path('university/faculty/depart/doctor/<int:pk>/', views.CommentView.as_view(), name='comment'),
 ]
 
 
