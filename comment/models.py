b05fd687576143d3e087f844e6a97920de1bc224
hmtcelik <abhmtcelik@gmail.com>
2022-01-30 21:58:02 +0300

    design uni page

--- comment/models.py
+++ comment/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Uni(models.Model):
     uni_name = models.CharField(max_length=50)
+    uni_img = models.ImageField(upload_to ='uploads/', blank=True, null=True)
 
     def __str__(self):
         return self.uni_name
