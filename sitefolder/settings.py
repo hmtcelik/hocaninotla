e8c55940933cf367cdd7b9e79912723590017837
hmtcelik <abhmtcelik@gmail.com>
2022-03-28 19:45:02 +0300

    term of services info

--- sitefolder/settings.py
+++ sitefolder/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)g!5iln2%cx+o@7^%1sz)!vor)bgh2rjgy4*+ksidj*f=il1o9' #SAKIN PAYLASMA BU SECRETLE
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
