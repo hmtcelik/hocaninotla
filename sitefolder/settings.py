1e4d8644853391589ed6cc1cd91cef6f48c2c8ac
hmtcelik <abhmtcelik@gmail.com>
2022-03-29 15:03:25 +0300

    settings.py

--- sitefolder/settings.py
+++ sitefolder/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-))u!yx7iiq5^4f3hp^!qtwv6j+0ak_#=i=9*6l0q-du^_k9n$ay'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['178.62.228.126']
     
 
 # Application definition
