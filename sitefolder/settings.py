f77a053d23f8e7fabd98ce2fa2b220457f12e233
hmtcelik <abhmtcelik@gmail.com>
2022-04-03 15:59:39 +0300

    revert settings

--- sitefolder/settings.py
+++ sitefolder/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from distutils.log import debug
 from django.contrib.messages import constants as messages
 from pathlib import Path
 import os
@@ -22,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-))u!yx7iiq5^4f3hp^!qtwv6j+0ak_#=i=9*6l0q-du^_k9n$ay'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['178.62.228.126', 'localhost']
+ALLOWED_HOSTS = []
     
 
 # Application definition
@@ -85,17 +86,7 @@ if DEBUG:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myprojectdb',
-        'USER': 'myproject_admin',
-        'PASSWORD': 'AqG_3$F_wmC3MG@g',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
+
 
 
 # Password validation
@@ -140,7 +131,6 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 # Default primary key field type
